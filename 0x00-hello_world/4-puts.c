f2f95ec60aa9b058dc27f465ae6be444963d1a8e
ItsKjaja <contact.kjaja@gmail.com>
2023-02-16 14:36:49 -0800

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
 
 /**
  *  main - prints "Programming is like building a multilingual puzzle"
- *  retur 0.
+ *  Return: always 0.
  */
  int main(void)
 
@@ -14,5 +14,5 @@
 	puts("\"Programming is like building a multilingual puzzle");
 
 
-	return(0);
+	return (0);
 }
