3b7c496ce10c6ed504dd17303e01f281a8b1a68f
ItsKjaja <contact.kjaja@gmail.com>
2023-04-05 13:48:02 -0700

    task0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,22 +5,22 @@
  * print elements of linked list
  * @h:header of the list
  *
- * Return: te numbers of nodes
+ * Return: the numbers of nodes
  */
-   size_t prin_ listint(const listint_t *h)
 
-{	size_t count_nodes = 0;
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count_nodes = 0;
+
 	const listint_t *current = h;
 
-	while
-	      
-	      (current != NULL)
-	      {
-		      printf ("%i\n", current->n);
-                     current = current->next;
-                      count_nodes++;
-	      }
-	return count_nodes;
+	while (current != NULL)
+	{
+		printf("%i\n", current->n);
+		current = current->next;
+		count_nodes++;
+	}
+	return (count_nodes);
 }
-	
 
