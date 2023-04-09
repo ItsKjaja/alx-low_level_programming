53041e7a47af2ca04423611241cc9d1bae3440fa
ItsKjaja <contact.kjaja@gmail.com>
2023-04-09 10:36:08 -0700

    task3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -11,11 +11,11 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 
- {
+{
 	unsigned long int st;
 
 
-	if (index > (sizeof(unsigned long int) *8 - 1))
+	if (index > (sizeof(unsigned long int) * 8 - 1))
 		return (-1);
 
 	st = 1 << index;
