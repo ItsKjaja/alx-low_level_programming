80942bd86bc1eb88819e75bacbd46b71f928b0b0
ItsKjaja <contact.kjaja@gmail.com>
2023-04-08 23:12:22 -0700

    task1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
-
- * print_binary - prints the binary representation
+ * print_binary - prints the binary notation
  * of a number.
  * @n: unsigned long int.
  *
- * Return: no return.
+ * Return: void
  */
 
  void print_binary(unsigned long int n)
@@ -22,7 +21,7 @@
 
 	}
 	else
-	
+
 	{
 		_putchar('0');
 	}
