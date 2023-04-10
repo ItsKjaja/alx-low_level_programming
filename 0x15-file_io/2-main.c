e2414d013916e6865052c3d14d29b2bcc14faa9c
ItsKjaja <contact.kjaja@gmail.com>
2023-04-09 21:55:51 -0700

    task2_append

--- 0x15-file_io/2-main.c
+++ 0x15-file_io/2-main.c
@@ -9,14 +9,13 @@
  */
 int main(int ac, char **av)
 {
-    int res;
-
-    if (ac != 3)
+ int res;
+ if (ac != 3)
     {
-        dprintf(2, "Usage: %s filename text\n", av[0]);
-        exit(1);
-    }
-    res = append_text_to_file(av[1], av[2]);
-    printf("-> %i)\n", res);
-    return (0);
+ dprintf(2, "Usage: %s filename text\n", av[0]);
+ exit(1);
+  }
+ res = append_text_to_file(av[1], av[2]);
+ printf("-> %i)\n", res);
+  return (0);
 }
