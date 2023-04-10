7d85dd13efc117c0d59b6fb5396d7e5cc7519e26
ItsKjaja <contact.kjaja@gmail.com>
2023-04-09 22:10:39 -0700

    task2_append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -3,7 +3,7 @@
  * append_text_to_file - appends text at the end of a file
  * @filename: appended file.
  * @text_content: infos that will append into the file.
- * Return: 1 on success and -1 on failure. 
+ * Return: 1 on success -1 on failure.
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
