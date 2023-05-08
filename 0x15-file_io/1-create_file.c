36eb05b11acf07570b55ef52fe129e90988b90e3
ItsKjaja <contact.kjaj@gmail.com>
2023-05-08 14:31:40 -0700

    create_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,24 +14,27 @@ int create_file(const char *filename, char *text_content)
 {
 	int fi, check, n = 0;
 
-	if (filename == 0)
+	if (!filename)
 		return (-1);
 
-	fi = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	fi = open(filename, O_RDWR | O_CREAT | O_TRUNC, 00600);
 
 	if (fi == -1)
 		return (-1);
 
 	if (text_content)
 	{
-		while (text_content[1] != 0)
-			n++;
-		check = write(fi, text_content, n);
+		for (check = 0; *(text_content + check);)
+			check++;
+		
+		n = write(fi, text_content, check);
 
-		if (check == -1)
-			return (-1);
+		if (n != check)
+		{
+		close(fi);
+		return (-1);
 	}
-
+	}
 	close(fi);
 	return (1);
 }
