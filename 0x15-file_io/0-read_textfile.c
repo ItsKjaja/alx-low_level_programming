6f1816b1dafe4554b976527ac7df45163f179e18
ItsKjaja <contact.kjaj@gmail.com>
2023-05-08 13:48:43 -0700

    update 0-read_textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,29 +13,32 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int file;
 	int nl;
 	int wr;
-	char *txt;
+	char *buf;
 
 	if (!filename || letters == 0)
 		return (0);
 	file = open(filename, O_RDONLY);
 	if (file < 0)
 		return (0);
-	txt = malloc(sizeof(char) * letters);
-	if (txt == NULL)
-	{ close(file);
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+	{ 
+		close(file);
 		return (0);
 	}
-	nl = read(file, txt, letters);
+	nl = read(file, buf, letters);
 	close(file);
 	if (nl < 0)
 	{
-		free(txt);
+		free(buf);
 		return (0);
 	}
-	wr = write(STDOUT_FILENO, txt, nl);
+	wr = write(STDOUT_FILENO, buf, nl);
 	if (wr <= 0)
 	{
-		free(txt);
-		return (wr);
+		free(buf);
+		return (0);
 	}
+	free(buf);
+	return (wr);
 }
