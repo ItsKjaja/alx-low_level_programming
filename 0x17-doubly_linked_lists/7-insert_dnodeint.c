01d12750828485dc555deb0767d32c31b9108ea7
ItsKjaja <contact.kjaj@gmail.com>
2023-06-15 18:15:24 -0700

    insert

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -4,22 +4,22 @@
 /**
  * insert_dnodeint_at_index - inserts a new node in a dlistint_t list.
  * at a given position
- * @head: the pointer to the head of the dlistint_t list.
- * index: the position to insert the new node
+ * @h: the pointer to the head of the dlistint_t list.
+ * @idx: index
  * @n: the integer for the new node to contain.
  *
  * Return: NULL in faillure otherwise the address of the new node.
  *
  */
 
-dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int index, int n)
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *tmp = *head, *new;
+	dlistint_t *tmp = *h, *new;
 
-	if (index == 0)
-		return (add_dnodeint(head, n));
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 
-	for (; index != 1; index--)
+	for (; idx != 1; idx--)
 	{
 		tmp = tmp->next;
 		if (tmp == NULL)
@@ -27,7 +27,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int index, int 
 	}
 
 	if (tmp->next == NULL)
-		return (add_dnodeint_end(head, n));
+		return (add_dnodeint_end(h, n));
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
