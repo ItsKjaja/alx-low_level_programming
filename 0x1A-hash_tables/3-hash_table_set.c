f51980c2d11ad618a277b0476da43e2244d94b93
ItsKjaja <contact.kjaj@gmail.com>
2023-07-07 09:12:19 -0700

    fixing error

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -16,7 +16,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht == NULL || *key == '\n' || *value == '\n')
 		return (0);
-	index = key_index((const unsigned char *)key, ht->size;
+
+	index = key_index((const unsigned char *)key, ht->size);
 	curr_node = ht->array[index];
 
 	if (!curr_node)
